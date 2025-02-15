69b1d71255810eac7514b41e3355debe2a616132
Neal Caren <neal.caren@gmail.com>
2025-02-15 10:19:19 -0500

    chore: Update SHA256 hash for pdfextractimages formula

--- Formula/pdfextractimages.rb
+++ Formula/pdfextractimages.rb
@@ -2,7 +2,7 @@ class Pdfextractimages < Formula
   desc "Extract high-quality images from PDF files while preserving metadata"
   homepage "https://github.com/nealcaren/pdfextractimages"
   url "https://github.com/nealcaren/pdfextractimages/archive/refs/tags/v1.0.0.tar.gz"
-  sha256 "THE_ACTUAL_SHA256_YOU_GENERATED"
+  sha256 "b3bcba20ef8c9678260eeb8670cc6f08cc3577426adb608521b97b7b0b16b12d"
   license "MIT"
 
   depends_on "python@3.8"
